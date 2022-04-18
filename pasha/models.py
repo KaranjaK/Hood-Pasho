51b9f7e82fcc014e61196c35ae542c267a493650
KK <karanjakelvin77@gmail.com>
2022-04-18 19:48:01 +0300

    It will create the four models necessary for the project

--- pasha/models.py
+++ pasha/models.py
@@ -1,4 +1,7 @@
+import email
 from django.db import models
+from unicodedata import CloudinaryField
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -7,7 +10,11 @@ class Neighborhood(models.Model):
     name = models.CharField(max_length=40)
     location = models.CharField(max_length=40)
     occupants = models.IntegerField()
-    admin = models.ForeignKey(Admin, on_delete=models.CASCADE)
+    admin = models.ForeignKey('Resident', on_delete=models.CASCADE)
+    hood_pic = CloudinaryField('image')
+    description = models.CharField(max_length=300)
+    health_contact = models.IntegerField(null=True, blank=True)
+    police_contact = models.IntegerField(null=True, blank=True)
 
     def __st__(self):
         return self
@@ -15,4 +22,16 @@ class Neighborhood(models.Model):
     def create_neighborhood(self):
         self.save()
 
-    
+# The User class
+class Resident(models.Model):
+    name = models.CharField(max_length=30)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    neighborhood = models.ForeignKey(Neighborhood, on_delete=models.SET_NULL, null=True, blank=True)
+    email = models.CharField(max_length=50)
+
+# The Business class
+class Business(models.Model):
+    name = models.CharField(max_length=40)
+    resident = models.ForeignKey(Resident, on_delete=models.SET_NULL, null = True, blank=True)
+    neighborhood = models.ForeignKey(Neighborhood, on_delete=models.SET_NULL, null = True, blank=True)
+    email = models.CharField(max_length=50)
