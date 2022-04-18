259969400efec5c838415ff1d67e9157c537f824
KK <karanjakelvin77@gmail.com>
2022-04-18 17:30:13 +0300

    It will install cloudinary

--- pasha/models.py
+++ pasha/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+# The Neighborhood model
+class Neighborhood(models.Model):
+    name = models.CharField(max_length=40)
+    location = models.CharField(max_length=40)
+    occupants = models.IntegerField()
+    admin = models.ForeignKey(Admin, on_delete=models.CASCADE)
+
+    def __st__(self):
+        return self
+
+    def create_neighborhood(self):
+        self.save()
+
+    
--- pasha/tests.py
+++ pasha/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Neighborhood
 
 # Create your tests here.
+# Neighborhood class tests
+class NeighborhoodTest(TestCase):
+    def setUp(self):
+        self.neighbor = Neighborhood('Umoja Phase1','Umoja-Nairobi',60)
+        self.neighbor.create_neighborhood
+    
+    # def test_instance(self):
+    #     self.assertTrue(isinstance(self.neighbor, Neighborhood))
