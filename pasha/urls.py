1c628c8ec16d38648fe8e5e28e1e886d9818b385
KK <karanjakelvin77@gmail.com>
2022-04-19 12:40:36 +0300

    It will create urls for profile and editprofile

--- pasha/urls.py
+++ pasha/urls.py
@@ -10,7 +10,8 @@ urlpatterns = [
     url(r'^account/', include('django.contrib.auth.urls')),
     url(r'^all-hoods/$', views.hoods, name='hood'),
     url(r'^new-hood/$', views.create_hood, name='new-hood'),
-    url(r'^profile/<username>/edit/$', views.edit_profile, name='edit-profile'),
     url(r'^join_hood/<id>$', views.join_hood, name='join-hood'),
     url(r'^leave_hood/<id>$', views.leave_hood, name='leave-hood'),
+    url(r'^profile/<username>$', views.profile, name='profile'),
+    url(r'^profile/<username>/edit/$', views.edit_profile, name='edit-profile'),
 ]
