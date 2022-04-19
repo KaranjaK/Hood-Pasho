ce8ba70a5c1ba1d8a5e113d92a596b06875a5b7b
KK <karanjakelvin77@gmail.com>
2022-04-19 16:18:35 +0300

    Heroku Deployment

--- hoodpasho/settings.py
+++ hoodpasho/settings.py
@@ -131,7 +131,11 @@ WSGI_APPLICATION = 'hoodpasho.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': '',
     }
 }
 # These are cloudinary Configurations 
