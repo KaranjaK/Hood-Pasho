d4fd13c784127d5cced48b74b500551738e658ef
KK <karanjakelvin77@gmail.com>
2022-04-19 11:19:19 +0300

    It will update the views for the index page

--- pasha/views.py
+++ pasha/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+# from .forms import
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from .models import Neighborhood, Resident, Business
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 
 # Create your views here.
+# Default page load
+# @login_required(login_url='login')
+def index(request):
+    return render(request, 'index.html')
