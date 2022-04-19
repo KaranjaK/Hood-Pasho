d3e1d037e8fa104ddad93f39039436653d094d4a
KK <karanjakelvin77@gmail.com>
2022-04-19 16:55:13 +0300

    Heroku Deployment

--- hoodpasho/settings.py
+++ hoodpasho/settings.py
@@ -130,7 +130,7 @@ WSGI_APPLICATION = 'hoodpasho.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
