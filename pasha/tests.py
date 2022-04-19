9c6c24a13b1cc26bf5cc9b8280ae838a68d50e1e
KK <karanjakelvin77@gmail.com>
2022-04-19 03:20:39 +0300

    It will create tests for the Neighborhood model

--- pasha/tests.py
+++ pasha/tests.py
@@ -1,12 +1,21 @@
 from django.test import TestCase
-from .models import Neighborhood
+from .models import Neighborhood, Resident, Business
 
 # Create your tests here.
 # Neighborhood class tests
 class NeighborhoodTest(TestCase):
+
+    # Method to setup a neighborhood for testing
     def setUp(self):
-        self.neighbor = Neighborhood('Umoja Phase1','Umoja-Nairobi',60)
+        self.neighbor = Neighborhood(23,'Umoja Phase1','Umoja-Nairobi',60)
         self.neighbor.create_neighborhood
     
-    # def test_instance(self):
-    #     self.assertTrue(isinstance(self.neighbor, Neighborhood))
+    # Method to test the instance of the neigborhood created
+    def test_instance(self):
+        self.assertTrue(isinstance(self.neighbor, Neighborhood))
+
+    # Method to test the creation of a new neighborhood
+    def test_create_neighborhood(self):
+        self.neighbor.create_neighborhood()
+        neighbor = Neighborhood.objects.all()
+        self.assertTrue(len(neighbor)>0)
