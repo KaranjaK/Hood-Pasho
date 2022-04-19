992d9710745a823ac440b7261dcdd96f9d44352b
KK <karanjakelvin77@gmail.com>
2022-04-19 04:44:55 +0300

    It will update the Neighborhood tests

--- pasha/tests.py
+++ pasha/tests.py
@@ -5,17 +5,30 @@ from .models import Neighborhood, Resident, Business
 # Neighborhood class tests
 class NeighborhoodTest(TestCase):
 
-    # Method to setup a neighborhood for testing
+    # Setting up a neighborhood for testing
     def setUp(self):
-        self.neighbor = Neighborhood(23,'Umoja Phase1','Umoja-Nairobi',60)
+        self.neighbor = Neighborhood('23','Umoja Phase1','Umoja-Nairobi',60)
         self.neighbor.create_neighborhood
     
-    # Method to test the instance of the neigborhood created
+    # Testing the instance of the neigborhood created
     def test_instance(self):
         self.assertTrue(isinstance(self.neighbor, Neighborhood))
 
-    # Method to test the creation of a new neighborhood
+    # Testing the creation of a new neighborhood
     def test_create_neighborhood(self):
         self.neighbor.create_neighborhood()
         neighbor = Neighborhood.objects.all()
         self.assertTrue(len(neighbor)>0)
+
+    # Testing the deleting of a neighborhood
+    def test_delete_neighborhood(self):
+        self.neighbor.delete_neighborhood()
+        neighbor = Neighborhood.objects.all()
+        self.assertTrue(len(neighbor)==0)
+    
+    # Testing the update of a neighborhood
+    def test_update_neighborhood(self):
+        new_hood = 'Donholm'
+        self.neighbor.update_neighborhood(self.neighbor.id, new_hood)
+        changed_hood = Neighborhood.objects.filter(id=23).update(name='Donhom')
+        self.assertTrue(len(changed_hood) > 0)
